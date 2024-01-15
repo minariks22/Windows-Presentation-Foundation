04ee9310e4bcbd0aa57ab550b7460bfc0e710a17
Samuel Minařík <minariks22@spstrutnov.cz>
2024-01-15 11:35:15 +0100

    správná kalkulačka

---
+++ kalkulačka WPF/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int PocitadloStisku { get; set; } = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Klik1(object sender, RoutedEventArgs e)
+        {
+
+            PocitadloStisku += 1;
+
+            xd.Content = PocitadloStisku;
+        }
+
+        private void Klik2 (object sender, RoutedEventArgs e)
+        {
+            PocitadloStisku += 2;
+            xd.Content = PocitadloStisku;
+        }
+    }
+}
