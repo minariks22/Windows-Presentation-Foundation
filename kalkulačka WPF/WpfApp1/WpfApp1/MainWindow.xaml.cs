49c1f08b3d3a1333b84a8b9e9d37c7897e17b616
MinarikS <minariks22@spstrutnov.cz>
2024-01-19 18:41:14 +0100

    Hotová klakulačka
    funkční kalkulačka

--- kalkulačka WPF/WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ kalkulačka WPF/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Reflection.Emit;
 using System.Text;
@@ -33,7 +34,6 @@ namespace WpfApp1
                 switch (buttonContent)
                 {
                     case "=":
-                        
                         EvaluateExpression();
                         break;
                     case "1/x":
@@ -48,8 +48,31 @@ namespace WpfApp1
                         // Odmocnina
                         CalculateSquareRoot();
                         break;
+                    case "+/-":
+                        ChangeSign();
+                        break;
+                    case "%":
+                        ApplyPercentage();
+                        break;
+                    case "+":
+                    case "-":
+                    case "*":
+                    case "/":
+                        AppendOperator(buttonContent);
+                        break;
+                    case ",":
+                        AppendDecimalSeparator();
+                        break;
+                    case "C":
+                        ClearContent();
+                        break;
+                    case "CE":
+                        ClearContent();
+                        break;
+                    case "<--":
+                        Backspace();
+                        break;
                     default:
-                        
                         xd.Content += buttonContent;
                         break;
                 }
@@ -87,6 +110,15 @@ namespace WpfApp1
             }
         }
 
+        private void Backspace()
+        {
+            if (xd.Content.ToString().Length > 0)
+            {
+                // Odstraní poslední znak
+                xd.Content = xd.Content.ToString().Substring(0, xd.Content.ToString().Length - 1);
+            }
+        }
+
         private void CalculateSquareRoot()
         {
 
@@ -96,10 +128,59 @@ namespace WpfApp1
             }
         }
 
+        private void AppendDecimalSeparator()
+        {
+            // Zkontroluje, zda výraz již neobsahuje desetinný oddělovač
+            if (!xd.Content.ToString().Contains("."))
+            {
+                // Pokud neobsahuje, přidá čárku
+                xd.Content += ".";
+            }
+        }
+
+            private void ChangeSign()
+        {
+            if (double.TryParse(xd.Content.ToString(), out double currentValue))
+            {
+                xd.Content = (-currentValue).ToString();
+            }
+        }
+
+        private void ApplyPercentage()
+        {
+            if (double.TryParse(xd.Content.ToString(), out double currentValue))
+            {
+                xd.Content = (currentValue / 100).ToString();
+            }
+        }
+
+        private void AppendOperator(string operatorSymbol)
+        {
+            // Přidá operátor k obsahu labelu
+            xd.Content += operatorSymbol;
+        }
+
+        private void ClearContent()
+        {
+            // Vymaže obsah labelu
+            xd.Content = string.Empty;
+        }
+
         private string Evaluate(string expression)
         {
+            try
+            {
+                // Nahradí čárky tečkami, aby bylo možné správně vyhodnotit výraz
+                expression = expression.Replace(",", ".");
 
-            return "Implementujte vyhodnocení výrazu";
+                DataTable table = new DataTable();
+                return Convert.ToDouble(table.Compute(expression, string.Empty)).ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Chyba při vyhodnocování výrazu: " + ex.Message);
+                return "Chyba";
+            }
         }
     }
 }
